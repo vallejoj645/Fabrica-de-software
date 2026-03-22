5ec40b7e14e1aa7c2cbfa18aa7cd2c0a8d0f9708
vallejoj645 <96347258+vallejoj645@users.noreply.github.com>
2026-03-22 18:19:50 -0500

    cambio de corredor

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
